aa180555cace19b961a95fa6d7ca579c4ea80527
khodetsky <94411255+khodetsky@users.noreply.github.com>
2023-07-26 23:56:20 +0000

    Deploying to gh-pages from @ khodetsky/postlib@272074cffe9796a62fb08229a6e04b09838325c0 🚀

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/postlib/posts">
+    <BrowserRouter basename="/postlib/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
