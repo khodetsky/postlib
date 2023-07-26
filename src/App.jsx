16fc7de2ecefe8804b7cbe30a3fb4d1bbf62aaa9
khodetsky <94411255+khodetsky@users.noreply.github.com>
2023-07-26 23:48:03 +0000

    Deploying to gh-pages from @ khodetsky/postlib@7efb8745a6acce616a80339bf3ef8ecf243ce9c4 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 // import { useState } from 'react'
+import { MainPage } from "./pages/MainPage/MainPage";
+import { CommentsPage } from "./pages/CommentsPage/CommentsPage";
+import { GlobalStyles } from "./GlobalStyles";
 
 export const App = () => {
 
   return (
     <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/posts" element={<MainPage />} />
+        <Route path="/comments/:postId" element={<CommentsPage />} />
+      </Routes>
     </>
   )
 }
